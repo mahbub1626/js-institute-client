abb0f4d4d4fe6d96440abf25880c58b9f54c8dbb
mahbub1626 <mahbub1626@gmail.com>
2022-10-27 23:57:01 +0600

    ans added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -4,11 +4,11 @@ const Blog = () => {
     return (
         <div>
             <h2>4.1 what is cors?</h2>
-            <p>Ans:</p>
+            <p>Ans: Cross-origin resource sharing is a method that allows restricted resources on a web page to be requested from another website.</p>
             <h2>4.2 Why are you using firebase? What other options do you have to implement authentication?</h2>
-            <p>Ans:</p>
+            <p>Ans: I am using firebase for authentication </p>
             <h2> 4.3 How does the private route work?</h2>
-            <p>Ans:</p>
+            <p>Ans: Protect a page with condigition, if condition fulfiled then aprove to visit this page</p>
             <h2>4.4 What is Node? How does Node work?</h2>
             <p>Ans:</p>
 
